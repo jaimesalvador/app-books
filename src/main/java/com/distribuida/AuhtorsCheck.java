29fd59ebf1009a32a4302e51dc24ff7628516871
Jaime Salvador <jaime.salvador@ideasybits.com>
2023-02-28 08:39:37 -0500

    actualizado

---
+++ src/main/java/com/distribuida/AuhtorsCheck.java
@@ -0,0 +1,22 @@
+package com.distribuida;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import java.time.LocalDateTime;
+
+@ApplicationScoped
+@Liveness
+public class AuhtorsCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse
+                .named("app-authors")
+                .withData("time", LocalDateTime.now().toString() )
+                .up()
+                .build();
+    }
+}
